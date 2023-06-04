be3554814e1bbf8ded10026bee4e1d71efc75114
Daniel Veloza <ciberronaldo@hotmail.com>
2023-06-04 16:44:51 -0500

    feat: [main] se agrego el enrutador de supplier

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from config.database import engine, Base
 
 from middlewares.error_handler import Errorhandler
 from routers.product import product_router
+from routers.supplier import supplier_router
 
 
 app = FastAPI()
 app.title = "Proyecto de CRUD de inventario - FastAPI"
 app.version = "0.0.1"
-app.description = "Este proyecto es un ejemplo de una API RESTful desarrollada con FastAPI que permite realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) en un modelo de inventariado \n Funcionalidades\n - Obtener los datos de los productos\n - Obtener un producto por su ID \n- Crear una nuevo producto en el inventario \n- Actualizar una producto existente\n - Eliminar un item del inventario\n"
+app.description = "Este proyecto es un ejemplo de una API RESTful desarrollada con FastAPI que permite realizar operaciones CRUD (Crear, Leer, Actualizar, Eliminar) en un modelo de inventariado \n Funcionalidades\n - Obtener los datos de los productos / proveedores\n - Obtener un producto / proveedore por su ID \n- Crear una nuevo producto / proveedor en el inventario \n- Actualizar una producto / proveedor existente\n - Eliminar un dato de proveedores o productos del inventario\n"
 
 Base.metadata.create_all(bind=engine)
 
@@ -22,4 +23,5 @@ def message():
 
 app.add_middleware(Errorhandler)
 app.include_router(product_router)
+app.include_router(supplier_router)
 
